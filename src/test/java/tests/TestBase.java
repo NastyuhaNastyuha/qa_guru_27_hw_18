c3ee007d6c99b16786e10ef1b97d33e893723827
Nastya Rybina <przh.17@gmail.com>
2024-07-02 22:18:33 +0600

    configuration for remote run added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,6 +15,8 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         WebDriverManager.chromedriver().setup();
     }
 
